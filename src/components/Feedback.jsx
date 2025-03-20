d7fb26246fd8ef0571716035ba7e62b5c6f959d0
JuliaCicer13 <137887271+JuliaCicer13@users.noreply.github.com>
2025-03-20 15:18:05 +0200

    g

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -1,29 +1,18 @@
-import styles from './Feedback.module.css';
+import style from './Feedback.module.css';
 
-export default function Feedback ({good, neutral, bad, total, positive}) {
+export default function Feedback ({good, bad, neutral, positive,total}) {
        
         return (
             <>
-            <ul className = {styles.list}>
-            <li className={styles.wrap}>
-               Good:{good}
-            </li>
-            <li className={styles.wrap}>
-                Neutral:{neutral} 
-            </li>
-            <li className={styles.wrap}>
-                Bad:{bad} 
-                </li>
-                <li className={styles.wrap}>
-                    Total Feedback: {total}
-                </li>
-                <li className={styles.wrap}>Positive Feedback: {positive}%</li>
-         </ul>
+            <ul className={style.list}>
+            <li className={style.wrap}>Good:{good}</li>
+            <li className={style.wrap}>Bad:{bad}</li>
+            <li className={style.wrap}>Neutral:{neutral}</li>
+            <li className={style.wrap}>Total:{total}</li>
+            <li className={style.wrap}>Positive{positive}</li>
+            </ul>
+           
             </>
         )
        }
-
-
-
-
 
--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,9 +1,10 @@
 import styles from './Notification.module.css';
 
 export default function Notification () {
-  return ( <p className={styles.notification}>
-    No feedback yet
-  </p>
+  return ( 
+       <>
+       <p className={styles.notification}>Not feedback yet</p>
+       </>
   );
 
 }
